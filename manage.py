cc8af7f4f9d806a4fe6cfb2b4f9e60e8347f1581
Gabriel França de Albuquerque Pernambuco <gfap@cesar.school>
2025-04-15 17:13:51 -0300

    sem erro no module projeto

--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@
 import os
 import sys
 
+# Adiciona a raiz do projeto ao sys.path (pasta atual do manage.py)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, BASE_DIR)
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PortaldoAluno.projeto.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,7 +19,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
