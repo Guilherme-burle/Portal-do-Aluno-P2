266ee582246e648f8cd02b111afafa360340eb4a
Pedro Ferraz <pvf@cesar.school>
2025-04-15 15:42:27 -0300

    ajustes

--- PortaldoAluno/Aplicativo/models.py
+++ PortaldoAluno/Aplicativo/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Cadastro(models.Model):
     nome = models.TextField(max_length=100, default='usuario_temp')
     email = models.EmailField(default='temp@email.com')
