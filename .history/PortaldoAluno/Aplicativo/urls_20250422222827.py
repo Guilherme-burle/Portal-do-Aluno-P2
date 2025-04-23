f8346edb4db97358e3102f307a1c98766f788a13
Pedro Ferraz <pvf@cesar.school>
2025-04-23 00:18:54 -0300

    urls

--- .history/PortaldoAluno/Aplicativo/urls_20250422222827.py
+++ .history/PortaldoAluno/Aplicativo/urls_20250422222827.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
-    path('add/', views.add_aluno, name='add'),
+    path('add/', views.add_aluno, name='add_aluno'),
     path('homeadm/', views.home_adm, name='homeadm'),
     path('excluir/<int:aluno_id>/', views.excluir_aluno, name='excluir'),
 
