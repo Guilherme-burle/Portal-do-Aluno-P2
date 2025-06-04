9a0373e6825e9d21864d79bfd0e3d256000a6d79
Victor de Matos Vilela <vrt1701@gmail.com>
2025-06-04 14:31:04 -0300

    ajeitando para fazer os testes

--- PortaldoAluno/manage.py
+++ PortaldoAluno/manage.py
@@ -3,11 +3,12 @@ import os
 import sys
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(BASE_DIR, 'PortaldoAluno'))
+sys.path.insert(0, BASE_DIR)
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projeto.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PortaldoAluno.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
