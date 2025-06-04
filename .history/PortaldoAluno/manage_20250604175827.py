4699ffabc5e56729d435b911236a43a655c8c5ea
Pedro Ferraz <pvf@cesar.school>
2025-06-04 18:07:54 -0300

    erro runserver

--- .history/PortaldoAluno/manage_20250604175827.py
+++ .history/PortaldoAluno/manage_20250604175827.py
@@ -8,7 +8,7 @@ sys.path.insert(0, BASE_DIR)
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projeto.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PortaldoAluno.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
